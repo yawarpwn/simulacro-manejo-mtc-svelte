7187feead4cd6452b3f49351b2411a1a0b6dc3bb
yawarpwn <john.kazz@gmail.com>
2024-12-09 18:16:05 -0500

    add correct response ui

--- src/lib/components/Quiz.svelte
+++ src/lib/components/Quiz.svelte
@@ -1,6 +1,7 @@
 <script>
 import { getQuestions } from '../data/get-questions'
 import { globalState, endApp } from '../store/questions.svelte'
+import confetti from 'canvas-confetti'
 import { MAX_TRIES } from '../constants'
 
 let { selectedCategory, progress } = globalState
@@ -14,12 +15,13 @@ let currentIndex = $state(0)
 let respondedQuestions = $state([])
 let answers = $state([])
 let selectAlternative = $state(null)
+let showResult = $state(false)
 
 // Estados computados
 const currentQuestion = $derived(questions[currentIndex])
 const alternatives = $derived(Object.entries(currentQuestion.alternatives))
 
-$inspect(progress)
+$inspect(selectAlternative)
 
 //Funciones
 
@@ -50,14 +52,8 @@ const getRandomQuestion = () => {
 const getImageUrl = (id) =>
   `https://sierdgtt.mtc.gob.pe/Content/img-data/img${id}.jpg`
 
-/**
- * @param {Event} event
- */
-const handleSubmit = (event) => {
-  event.preventDefault()
-
-  if (!selectAlternative) return
-
+const nextQuestion = () => {
+  showResult = false
   // Guardar respuesta
   answers = [
     ...answers,
@@ -77,6 +73,27 @@ const handleSubmit = (event) => {
   globalState.progress++
 
   getRandomQuestion()
+}
+
+/**
+ * @param {Event} event
+ */
+const handleSubmit = (event) => {
+  event.preventDefault()
+
+  if (!selectAlternative) return
+  if (selectAlternative === currentQuestion.correctAlternative) {
+    confetti()
+  }
+  showResult = true
+}
+
+/**
+ * @param {Event} event
+ */
+const handleNextQuestion = (event) => {
+  event.preventDefault()
+  nextQuestion()
 }
 </script>
 
@@ -107,28 +124,31 @@ const handleSubmit = (event) => {
     >
       <div class="w-full">
         <div class="flex flex-col gap-4">
-          {#each alternatives as [key, value]}
+          {#each alternatives as [letter, value]}
             <label
-              aria-checked={key === selectAlternative}
-              class="relative flex cursor-pointer items-center overflow-hidden rounded-lg border-2 border-neutral-500 bg-black pl-8 leading-6 data-[state=false]:opacity-50 data-[state=true]:opacity-100"
-              data-state={key === selectAlternative}
-              onclick={() => onSelectAlternative(key)}
+              aria-checked={selectAlternative === letter}
+              class:show-result={showResult}
+              class="relative flex cursor-pointer items-center overflow-hidden rounded-lg border border-neutral-500 bg-black pl-8 leading-6 data-[state=false]:opacity-50 data-[state=true]:opacity-100"
+              data-is-correct={currentQuestion.correctAlternative === letter}
+              data-state={selectAlternative === letter}
+              onclick={() => onSelectAlternative(letter)}
+              aria-disabled={showResult}
             >
               <button
                 class="absolute bottom-0 left-0 top-0 w-8 min-w-8 bg-[#222] text-[#fff]"
                 type="button"
-                data-state={key === selectAlternative}
-                value={key}
+                data-state={letter === selectAlternative}
+                value={letter}
               >
-                {key.toUpperCase()}</button
+                {letter.toUpperCase()}</button
               >
 
               <input
                 type="radio"
                 aria-hidden="true"
-                checked={key === selectAlternative}
+                checked={letter === selectAlternative}
                 {value}
-                tabIndex={-1}
+                tabindex="-1"
                 class="absolute h-14 w-8 -translate-x-full opacity-0"
               />
               <span class="p-4">{value}</span>
@@ -139,18 +159,63 @@ const handleSubmit = (event) => {
     </ul>
   </div>
 
-  <form onsubmit={handleSubmit} class="fixed bottom-0 left-0 right-0 h-[76px]">
-    <div
-      class="mx-auto flex h-full max-w-3xl items-center justify-end rounded-tl-lg rounded-tr-lg bg-[#1e2229] p-4"
+  {#if !showResult}
+    <form
+      onsubmit={handleSubmit}
+      class="fixed bottom-0 left-0 right-0 h-[76px]"
     >
-      <button
-        class="h-full w-full cursor-pointer rounded-md bg-primary px-4 py-2 text-black disabled:pointer-events-none disabled:opacity-20"
-        aria-disabled={!selectAlternative}
-        type="submit"
-        disabled={!selectAlternative}
+      <div
+        class="mx-auto flex h-full max-w-3xl items-center justify-end rounded-tl-lg rounded-tr-lg bg-[#1e2229] p-4"
       >
-        Siguiente
-      </button>
-    </div>
-  </form>
+        <button
+          class="h-full w-full cursor-pointer rounded-md bg-primary px-4 py-2 text-black disabled:pointer-events-none disabled:opacity-20"
+          aria-disabled={!selectAlternative}
+          type="submit"
+          disabled={!selectAlternative}
+        >
+          Responder
+        </button>
+      </div>
+    </form>
+  {:else}
+    <form
+      onsubmit={handleNextQuestion}
+      class="fixed bottom-0 left-0 right-0 h-[76px]"
+    >
+      <div
+        class="mx-auto flex h-full max-w-3xl items-center justify-end rounded-tl-lg rounded-tr-lg bg-[#1e2229] p-4"
+      >
+        <button
+          class="flex h-full cursor-pointer items-center gap-2 rounded-md bg-primary px-4 py-2 text-black disabled:pointer-events-none"
+          >Siguiente <svg
+            width="1em"
+            height="1em"
+            fill="none"
+            viewBox="0 0 12 12"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              fill="#13161C"
+              d="M6 0 4.943 1.058 9.128 5.25H0v1.5h9.128l-4.185 4.193L6 12l6-6z"
+            ></path></svg
+          >
+        </button>
+      </div>
+    </form>
+  {/if}
 </div>
+
+<style>
+label[aria-disabled='true'] {
+  cursor: not-allowed;
+  pointer-events: none;
+}
+.show-result {
+}
+.show-result[data-is-correct='true'] {
+  border-color: hsl(var(--primary));
+}
+
+.show-result[data-is-correct='false'] {
+  border-color: hsl(var(--destructive));
+}
+</style>
