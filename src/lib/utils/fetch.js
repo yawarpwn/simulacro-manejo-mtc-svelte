288f7e46d431abbfe8ca26520e03ea932e71e609
yawarpwn <john.kazz@gmail.com>
2024-12-16 23:04:37 -0500

    upate

--- src/lib/utils/fetch.js
+++ src/lib/utils/fetch.js
@@ -1,17 +1,17 @@
-import a1Questions from "../data/a1.json";
+import a1Questions from '../data/a1.json'
 
 /**
  * @param {string} category
  */
 const replaceMultipleSpaceWithSllipsis = (category) =>
-  category.replace(/_/, "").replace(/\s{3,}/g, " ________ ");
+  category.replace(/_/, '').replace(/\s{3,}/g, ' ________ ')
 
 /**
  * @param {string} str
  */
-const format = (str) => str.slice(3, str.length);
+const format = (str) => str.slice(3, str.length)
 
-export function getQuestions({ selectedCategory }) {
+export function getQuestions() {
   const mappedQuestions = a1Questions.map((question) => {
     return {
       id: question.index,
@@ -24,8 +24,8 @@ export function getQuestions({ selectedCategory }) {
       question: replaceMultipleSpaceWithSllipsis(question.pregunta),
       correctAlternative: question.respuesta,
       hasImage: Boolean(question.image),
-    };
-  });
+    }
+  })
 
-  return mappedQuestions;
+  return mappedQuestions
 }
